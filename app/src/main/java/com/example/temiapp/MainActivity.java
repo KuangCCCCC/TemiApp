4d2d58394e9e9566e875533ab55e3af0f332dc83
KuangCCCCC <kuangyong0408@gmail.com>
2024-11-22 18:20:34 +0800

    更新了目前決定的功能，畫面還沒拉好

--- app/src/main/java/com/example/temiapp/MainActivity.java
+++ app/src/main/java/com/example/temiapp/MainActivity.java
@@ -13,8 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Button navigateButton;
-    private Button patrolButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
